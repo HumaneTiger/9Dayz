d9309175f79f19e7d0abc2402add1fc83a3d0272
Humane Tiger <121488080+HumaneTiger@users.noreply.github.com>
2026-01-02 16:31:47 +0100

    Refactor test execution setup to use PowerShell script and improve Chrome launch parameters

--- run-tests.bat
+++ run-tests.bat
@@ -1,3 +1,4 @@
 @echo off
 start python server.py
-chrome --disable-session-crashed-bubble --no-first-run "http://127.0.0.1:8080/index.html?startPlayback=test-run-2"
+chrome --new-instance --user-data-dir=./test-chrome-profile --disable-session-crashed-bubble --no-first-run "http://127.0.0.1:8080/index.html?startPlayback=test-run-2"
+rmdir /s /q test-chrome-profile >nul 2>&1
