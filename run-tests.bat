74052d367823d89ba71c26352a1371538d13a077
Humane Tiger <121488080+HumaneTiger@users.noreply.github.com>
2026-01-02 17:15:47 +0100

    remove the test attempt

--- run-tests.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start python server.py
-chrome --new-instance --user-data-dir=./test-chrome-profile --disable-session-crashed-bubble --no-first-run "http://127.0.0.1:8080/index.html?startPlayback=test-run-2"
-rmdir /s /q test-chrome-profile >nul 2>&1
