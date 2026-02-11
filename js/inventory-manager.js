02134ec503e4e50814555203a4b6ec1d4ade8c2b
Humane Tiger <121488080+HumaneTiger@users.noreply.github.com>
2026-02-11 22:01:37 +0100

    create new backward compatible inventory manager to seperate concerns

---
+++ js/inventory-manager.js
@@ -0,0 +1,172 @@
+import Events, { EVENTS } from './events.js';
+import itemsWeaponsDefinitions from '../data/definitions/items-weapons-definitions.js';
+import characterDefinitions from '../data/definitions/character-definitions.js';
+import ItemUtils from '../data/utils/item-utils.js';
+
+// Destructure items/weapons definitions
+const { items } = itemsWeaponsDefinitions;
+
+var inventory = {
+  items: new Array(),
+  itemNumbers: 0,
+  leftHand: null,
+  rightHand: null,
+};
+
+var inventoryBatch = {
+  active: false,
+  oldTotal: 0,
+};
+
+export default {
+  getInventory: function () {
+    return inventory;
+  },
+
+  getInventoryItemNumbers: function () {
+    return inventory.itemNumbers;
+  },
+
+  getInventoryPresets: function (character) {
+    return characterDefinitions[character]?.inventoryPreset || {};
+  },
+
+  /**
+   * Begin batching inventory changes
+   * Captures current total, multiple adds will emit single event
+   */
+  beginInventoryBatch: function () {
+    inventoryBatch.active = true;
+    inventoryBatch.oldTotal = inventory.itemNumbers;
+  },
+
+  /**
+   * End batching and emit single INVENTORY_CHANGED event
+   */
+  endInventoryBatch: function () {
+    inventoryBatch.active = false;
+    const newTotal = inventory.itemNumbers;
+
+    // EVENT: Notify that inventory changed
+    Events.emit(EVENTS.INVENTORY_CHANGED, {
+      oldTotal: inventoryBatch.oldTotal,
+      newTotal: newTotal,
+    });
+  },
+
+  addWeaponToInventory: function (item, addAmount, setWeaponProps) {
+    const amount = parseInt(addAmount),
+      setDamage = setWeaponProps.damage,
+      setProtection = setWeaponProps.protection,
+      setDurability = setWeaponProps.durability,
+      itemProps = items[item];
+
+    const oldTotal = this.getWeaponTotal();
+
+    if (inventory.items[item] !== undefined) {
+      // weapon was added to inventory before
+      inventory.items[item].amount += amount;
+      inventory.items[item].amount < 0 ? (inventory.items[item].amount = 0) : false;
+
+      setDamage ? (inventory.items[item].damage = setDamage) : false;
+      setProtection ? (inventory.items[item].protection = setProtection) : false;
+
+      if (setDurability !== undefined) {
+        inventory.items[item].durability += setDurability;
+        if (inventory.items[item].durability <= 0) {
+          // remove and reset inventory weapon props
+          inventory.items[item].amount = 0;
+          inventory.items[item].damage = ItemUtils.calcItemDamage(item);
+          inventory.items[item].protection = ItemUtils.calcItemProtection(item);
+          inventory.items[item].durability = 0;
+        }
+      }
+    } else if (itemProps !== undefined) {
+      // weapon is added first time to inventory
+      inventory.items[item] = {
+        type: itemProps[0],
+        name: item,
+        amount: amount,
+        damage: setDamage || ItemUtils.calcItemDamage(item),
+        protection: setProtection || ItemUtils.calcItemProtection(item),
+        durability: setDurability,
+      };
+    } else {
+      console.log('adding weapon "' + item + '" to inventory failed');
+    }
+
+    const newTotal = this.getWeaponTotal();
+
+    // EVENT: Emit if not batching
+    if (!inventoryBatch.active) {
+      Events.emit(EVENTS.WEAPON_CHANGED, {
+        oldTotal: oldTotal,
+        newTotal: newTotal,
+      });
+    }
+  },
+
+  getWeaponTotal: function () {
+    let total = 0;
+    for (let item in inventory.items) {
+      if (inventory.items[item].type === 'extra' && inventory.items[item].amount > 0) {
+        total += 1;
+      }
+    }
+    return total;
+  },
+
+  addItemToInventory: function (item, addAmount) {
+    const amount = parseInt(addAmount),
+      itemProps = items[item];
+
+    const oldTotal = inventory.itemNumbers;
+
+    if (inventory.items[item] !== undefined) {
+      // item was added to inventory before
+      inventory.items[item].amount += amount;
+      inventory.items[item].amount < 0 ? (inventory.items[item].amount = 0) : false;
+    } else if (itemProps !== undefined) {
+      // item is added first time to inventory
+      inventory.items[item] = {
+        type: itemProps[0],
+        name: item,
+        amount: amount,
+        damage: ItemUtils.calcItemDamage(item), // props for battle mode
+        protection: ItemUtils.calcItemProtection(item), // props for battle mode
+      };
+      // emit FIRST_ITEM_ADDED event for almanac tracking
+      Events.emit(EVENTS.FIRST_ITEM_ADDED, {
+        item: item,
+      });
+    } else {
+      console.log('adding item "' + item + '" to inventory failed');
+    }
+    this.calcTotalInventoryItems();
+
+    // EVENT: Emit if not batching
+    if (!inventoryBatch.active) {
+      Events.emit(EVENTS.INVENTORY_CHANGED, {
+        oldTotal: oldTotal,
+        newTotal: inventory.itemNumbers,
+      });
+    }
+  },
+
+  calcTotalInventoryItems: function () {
+    inventory.itemNumbers = 0;
+    for (let item in inventory.items) {
+      if (
+        inventory.items[item].type !== 'extra' &&
+        inventory.items[item].amount &&
+        inventory.items[item].amount > 0
+      ) {
+        inventory.itemNumbers += inventory.items[item].amount;
+      }
+    }
+  },
+
+  calcItemProps: function (item, character) {
+    return ItemUtils.calcItemProps(item, character);
+  },
+};
