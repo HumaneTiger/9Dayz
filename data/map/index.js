7e071475ca08769caa3415ab09393d0c17a2921e
Humane Tiger <121488080+HumaneTiger@users.noreply.github.com>
2026-02-12 19:08:55 +0100

    add location-instances.js with target locations data

--- data/map/index.js
+++ data/map/index.js
@@ -1,3 +1,4 @@
 export { default as BuildingInstances } from './building-instances.js';
 export { default as ZombieInstances } from './zombie-instances.js';
 export { default as PathInstances } from './path-instances.js';
+export { default as LocationInstances } from './location-instances.js';
---
+++ data/map/location-instances.js
@@ -0,0 +1,12 @@
+export default {
+  targetLocations: {
+    'Lakeside Camp Resort': [5, 37],
+    Rocksprings: [22, 34],
+    'Haling Cove': [16, 8],
+    Litchfield: [15, 23],
+    Greenleafton: [33, 35],
+    'Billibalds Farm': [40, 30],
+    'Camp Silverlake': [28, 22],
+    'Harbor Gas Station': [34, 16],
+  },
+};
